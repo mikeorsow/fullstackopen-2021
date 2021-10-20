c71de4b91a2bed061e51330e3a3f31e5159e6eb0
Mike Orsow <mike.orsow@gmail.com>
2021-10-19 20:44:52 -0700

    Add app files and course component

---
+++ Part 2/courseinfo2/src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const Course = ({ id, name, parts }) => {
+  return (
+    <div>
+      <Header name={ name } />
+      <Content parts={ parts } />
+      <Total parts={ parts } />
+    </div>
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>    
+  )
+}
+
+const Total = ({ parts }) => {
+  const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={ parts[0] } />
+      <Part part={ parts[1] } />
+      <Part part={ parts[2] } />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course {...course} />
+    </div>
+  )  
+}
+
+export default App;
