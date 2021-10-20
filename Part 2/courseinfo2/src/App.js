ca7272390e95bb99b498fd2e044d9413a1ea4fe7
Mike Orsow <mike.orsow@gmail.com>
2021-10-19 23:21:21 -0700

    Add display multiple courses

--- Part 2/courseinfo2/src/App.js
+++ Part 2/courseinfo2/src/App.js
@@ -16,7 +16,8 @@ const Header = ({ name }) => {
   )
 }
 
-const Part = ({ part }) => {
+const Part = ({part}) => {
+  console.log('part', part)
   return (
     <p>
       {part.name} {part.exercises}
@@ -32,41 +33,63 @@ const Total = ({ parts }) => {
 }
 
 const Content = ({ parts }) => {
+  const allParts = parts.map(part => <Part part={ part } />);
   return (
     <div>
-      <Part part={ parts[0] } />
-      <Part part={ parts[1] } />
-      <Part part={ parts[2] } />
+      {allParts}
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  const allCourses = courses.map(course => <Course course={course} />)
   return (
     <div>
-      <Course course={course} />
+      {allCourses}
     </div>
   )  
 }
