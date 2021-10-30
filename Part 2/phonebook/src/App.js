78457b944c6ca49bacafdd95432c4d6373f16521
Mike Orsow <mike.orsow@gmail.com>
2021-10-29 21:39:40 -0700

    Add import of refactored services module

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import contactService from './services/contacts'
 
 const Contact = ( {filteredContacts} ) => {
   return (
@@ -39,46 +39,43 @@ const ContactForm = ({ tryAddContact, newName, handleNameChange, newNumber, hand
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [contacts, setContacts] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ searchTerm, setSearchTerm ] = useState('')
   
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+  useEffect( () => {
+    contactService
+      .getAll()
+      .then(initialContacts => {
+        setContacts(initialContacts)
       })
-  }
-  useEffect(hook, [])
-  console.log('render', persons.length, 'contacts')
+  }, [])
+
   const tryAddContact = (event) => {
     event.preventDefault()
     newName.trim() === '' 
       ? alert('Name is required')
-      : persons.find(person => person.name.toLowerCase() === newName.toLowerCase().trim())
+      : contacts.find(contact => contact.name.toLowerCase() === newName.toLowerCase().trim())
       ? alert(`${newName} is already in your contact list`)
       : newNumber.trim() === ''
       ? alert('Number is required')
       : addContact()
   }
   const addContact = () => {
-    const personObject = {
+    const contactObject = {
        name: newName,
        number: newNumber,
-       id: persons.length + 1
+       id: contacts.length + 1
     }
-    setPersons(persons.concat(personObject))
+    setContacts(contacts.concat(contactObject))
     setNewName('')
     setNewNumber('')
   }
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleSearchChange = (event) => setSearchTerm(event.target.value)
-  const filteredContacts = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
   return (
     <div>
